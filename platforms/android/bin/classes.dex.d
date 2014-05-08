5776558434246e66e3f216f8b77ad6bfd1c0395d
hutiaxi <547897116@qq.com>
2014-05-08 20:52:05 +0800

    本地存储

---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+D:\storage\platforms\android\bin\classes.dex : \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\e07120712\storage\MyStorage.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\e07120712\storage\BuildConfig.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\e07120712\storage\R$attr.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\e07120712\storage\R$drawable.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\e07120712\storage\R$string.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\e07120712\storage\R$xml.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\e07120712\storage\R.class \
+D:\storage\platforms\android\bin\dexedLibs\classes-0b060cce94409c43e219c5099fbf5c92.jar \
